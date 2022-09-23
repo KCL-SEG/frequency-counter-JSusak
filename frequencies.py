9082ebe0c94be292ff1651523cd6ba18b309c18e
Josh <joshuasusak@gmail.com>
2022-09-23 15:15:35 +0100

    Add frequencies solution.
    As the keys have to be in string form, I decided to traverse through the list, changing any non-strings into a string. This makes it easier to collate the results, simply by using the .count() method on the key!

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,15 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    # Fill up the dictionary with initial values. If the value/string representation not in dict, add it and skip
+    # subsequent values.
+    for i in range(len(items)):
+        if type(items[i]) != "str":
+            items[i] = str(items[i])
+    for val in items:
+        if val not in frequencies or str(val) not in frequencies:
+            frequencies[str(val)] = 0
+    for(key) in frequencies:
+        frequencies[key] += items.count(key)
+
     return frequencies
